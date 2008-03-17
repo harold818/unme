2b2fb537b1d9206c72b4780d97c3f42a1249f04a
jacobcarpenter <jacobcarpenter@83d443b3-9a48-0410-a4bc-37dee8c7bf04>
2008-03-17 05:55:54 +0000

    Added DictionaryUtility.

---
+++ trunk/src/Unme.Common/DictionaryUtility.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace Unme.Common
+{
+	public static class DictionaryUtility
+	{
+		// Note: Implementations of IDictionary<TKey, TValue> can vary in whether they allow key
+		// to be a null reference. See: http://msdn2.microsoft.com/en-us/library/zyxt2e2h.aspx
+
+		public static TValue GetValueOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key)
+		{
+			if (dictionary == null)
+				throw new ArgumentNullException("dictionary");
+
+			TValue value;
+			dictionary.TryGetValue(key, out value);
+			return value;
+		}
+
+		// null coalescing (??) should be used to provide a non-standard default value for reference types
+		public static TValue GetValueOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue defaultValue)
+			where TValue : struct
+		{
+			if (dictionary == null)
+				throw new ArgumentNullException("dictionary");
+
+			TValue value;
+			return dictionary.TryGetValue(key, out value) ? value : defaultValue;				
+		}
+	}
+}
