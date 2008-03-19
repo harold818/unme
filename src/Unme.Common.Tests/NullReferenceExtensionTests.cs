a82876477f59001fc9ba7996e79ae37af0a93d27
sjalex <sjalex@83d443b3-9a48-0410-a4bc-37dee8c7bf04>
2008-03-19 07:52:57 +0000

    Added GetPropertyName static method, ToSequence extension method, and NullReferenceExtension.cs

---
+++ src/Unme.Common.Tests/NullReferenceExtensionTests.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+using Unme.Common.NullReferenceExtension;
+using Xunit;
+
+namespace Unme.Common.Tests
+{
+	public class NullReferenceExtensionTests
+	{
+		[Fact]
+		public void IfNotNullAction()
+		{
+			string foo = "bar";
+			int i = 0;
+			bool actionExecuted = false;
+			foo.IfNotNull((Action<string>) (s => actionExecuted = true));
+
+			Assert.True(actionExecuted);
+		}
+
+		[Fact]
+		public void IfNotNullActionWithNullValue()
+		{
+			string foo = null;
+			bool actionExecuted = false;
+			foo.IfNotNull((Action<string>) (s => actionExecuted = true));
+
+			Assert.False(actionExecuted);
+		}
+
+		[Fact]
+		public void IfNotNullFunc()
+		{
+			string foo = "Foo";
+			Assert.Equal("FooBar", foo.IfNotNull(s => String.Concat(s, "Bar")));
+		}
+
+		[Fact]
+		public void IfNotNullFuncWithNullValue()
+		{
+			string foo = null;
+			Assert.Null(foo.IfNotNull(s => String.Concat(s, "Bar")));
+		}
+	}
+}
