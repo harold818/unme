18aa4458477869ff7822d51b37a30c8ad29b467b
sjalex <sjalex@83d443b3-9a48-0410-a4bc-37dee8c7bf04>
2008-03-19 05:07:14 +0000

    Added AssemblyInfo.cs, use <Compile Include="Unme.Common\**\*.cs" Exclude="Unme.Common\Properties\*.cs" /> to exclude conflicting AssemblyInfo.cs

---
+++ src/Unme.Common/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Unme.Common")]
+[assembly: AssemblyDescription(".NET Utility library.")]
+[assembly: AssemblyProduct("Unme.Common")]
+[assembly: AssemblyCopyright("Licensed under the MIT license.")]
+[assembly: Guid("9a63f23f-2c56-490e-a235-cab730935d92")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
