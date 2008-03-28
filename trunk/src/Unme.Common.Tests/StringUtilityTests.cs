34f12eaf15d5ec0b59fdba865e7d805cf33c98dc
sjalex <sjalex@83d443b3-9a48-0410-a4bc-37dee8c7bf04>
2008-03-28 21:01:56 +0000

    Added StringUtility extension method IsNullOrEmpty

---
+++ trunk/src/Unme.Common.Tests/StringUtilityTests.cs
@@ -0,0 +1,19 @@
+﻿
+using Xunit;
+using XunitExt;
+using System;
+
+namespace Unme.Common.Tests
+{
+	public class StringUtilityTests
+	{
+		[Theory,
+		InlineData(null, true),
+		InlineData("", true),
+		InlineData("foo", false)]
+		public void IsNullOrEmpty(string value, bool expectedResult)
+		{
+			Assert.Equal(expectedResult, value.IsNullOrEmpty());
+		}
+	}
+}
