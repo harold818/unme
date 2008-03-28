6334d27764104fad2062b7b19bfe9a0309fc7c6f
sjalex <sjalex@83d443b3-9a48-0410-a4bc-37dee8c7bf04>
2008-03-28 21:01:56 +0000

    Added StringUtility extension method IsNullOrEmpty

---
+++ src/Unme.Common/StringUtility.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+namespace Unme.Common
+{
+	/// <summary>
+	/// Utility methods for strings.
+	/// </summary>
+	public static class StringUtility
+	{
+		/// <summary>
+		/// Indicates whether the specified System.String object is null or an System.String.Empty string.
+		/// </summary>
+		/// <param name="str">A System.String reference.</param>
+		/// <returns>
+		/// true if the value parameter is null or an empty string (""); otherwise, false.
+		/// </returns>
+		public static bool IsNullOrEmpty(this string str)
+		{
+			return String.IsNullOrEmpty(str);
+		}
+	}
+}
