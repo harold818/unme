88557805e3f69d36e555cada7d33b42f3237e461
sjalex <sjalex@83d443b3-9a48-0410-a4bc-37dee8c7bf04>
2008-05-30 23:51:32 +0000

    Added EventUtility.

---
+++ trunk/src/Unme.Common/EventUtility.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Runtime.CompilerServices;
+
+namespace Unme.Common
+{
+	public static class EventUtility
+	{
+		[MethodImpl(MethodImplOptions.NoInlining)]
+		public static void Raise(this EventHandler handler, object sender)
+		{
+			if (handler != null)
+				handler(sender, EventArgs.Empty);
+		}
+
+		[MethodImpl(MethodImplOptions.NoInlining)]
+		public static void Raise<T>(this EventHandler<T> handler, object sender, T e) where T : EventArgs
+		{
+			if (handler != null)
+				handler(sender, e);
+		}
+	}
+}
