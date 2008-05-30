14a6332d306e3c1c0959ab85339ebf566c792d1f
sjalex <sjalex@83d443b3-9a48-0410-a4bc-37dee8c7bf04>
2008-05-30 23:51:32 +0000

    Added EventUtility.

---
+++ src/Unme.Common.Tests/EventUtilityTests.cs
@@ -0,0 +1,67 @@
+﻿
+using System;
+using NUnit.Framework;
+
+namespace Unme.Common.Tests
+{
+	[TestFixture]
+	public class EventUtilityTests
+	{
+		[Test]
+		public void Raise()
+		{
+			bool eventFired = false;
+			var foo = new Foo();
+
+			foo.MyEvent += (sender, args) =>
+			{
+				eventFired = true;
+				Assert.AreEqual(foo, sender);
+				Assert.AreEqual(EventArgs.Empty, args);
+			};
+
+			foo.FireEvent();
+
+			Assert.IsTrue(eventFired);
+		}
+
+		[Test]
+		public void Raise_Generic()
+		{
+			bool eventFired = false;		
+			var foo = new Foo();
+
+			foo.MyGenericEvent += (sender, args) =>
+			{
+				eventFired = true;
+				Assert.AreEqual(foo, sender);
+				Assert.AreEqual("Custom", ((MyEventArgs)args).CustomProperty);
+			};
+
+			foo.FireGenericEvent();
+
+			Assert.IsTrue(eventFired);			
+		}
+
+		class Foo
+		{
+			public event EventHandler MyEvent;
+			public event EventHandler<MyEventArgs> MyGenericEvent;
+
+			public void FireEvent()
+			{
+				MyEvent.Raise(this);
+			}
+
+			public void FireGenericEvent()
+			{
+				MyGenericEvent.Raise(this, new MyEventArgs() { CustomProperty = "Custom" });
+			}
+		}
+
+		class MyEventArgs : EventArgs
+		{
+			public string CustomProperty { get; set; }
+		}
+	}
+}
