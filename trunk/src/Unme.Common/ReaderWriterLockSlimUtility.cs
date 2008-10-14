b1ec397485abea69262132a47c4965dc72b6faf4
sjalex <sjalex@83d443b3-9a48-0410-a4bc-37dee8c7bf04>
2008-10-14 00:44:30 +0000

    Added IfNotNull with default value. Added scoped ReaderWriterLockSlimUtility.

---
+++ trunk/src/Unme.Common/ReaderWriterLockSlimUtility.cs
@@ -0,0 +1,48 @@
+﻿
+using System;
+using System.Threading;
+
+namespace Unme.Common
+{
+	/// <summary>
+	/// Convenience methods for the ReaderWriterLockSlim type.
+	/// </summary>
+	public static class ReaderWriterLockSlimUtility
+	{
+		/// <summary>
+		/// Convenience method providing a Scoped write lock.
+		/// </summary>
+		public static Scope ScopedEnterWriteLock(this ReaderWriterLockSlim readerWriterLock)
+		{
+			if (readerWriterLock == null)
+				throw new ArgumentNullException("readerWriterLock");
+
+			readerWriterLock.EnterWriteLock();
+			return Scope.Create(readerWriterLock.ExitWriteLock);
+		}
+
+		/// <summary>
+		/// Convenience method providing a Scoped read lock.
+		/// </summary>
+		public static Scope ScopedEnterReadLock(this ReaderWriterLockSlim readerWriterLock)
+		{
+			if (readerWriterLock == null)
+				throw new ArgumentNullException("readerWriterLock");
+
+			readerWriterLock.EnterReadLock();
+			return Scope.Create(readerWriterLock.ExitReadLock);
+		}
+
+		/// <summary>
+		/// Convenience method providing a Scoped upgradeable read lock.
+		/// </summary>
+		public static Scope ScopedEnterUpgradeableReadLock(this ReaderWriterLockSlim readerWriterLock)
+		{
+			if (readerWriterLock == null)
+				throw new ArgumentNullException("readerWriterLock");
+
+			readerWriterLock.EnterUpgradeableReadLock();
+			return Scope.Create(readerWriterLock.ExitUpgradeableReadLock);
+		}
+	}
+}
