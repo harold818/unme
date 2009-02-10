478acb172186c02ec98e9ab731cb5a1ee6f6705e
sjalex <sjalex@83d443b3-9a48-0410-a4bc-37dee8c7bf04>
2009-02-10 04:40:23 +0000

    Under CLR 2.0’s stronger memory model, this is no longer possible; there is no bug in copying the event delegate to a temporary local variable.

--- src/Unme.Common/EventUtility.cs
+++ src/Unme.Common/EventUtility.cs
@@ -6,14 +6,12 @@ namespace Unme.Common
 {
 	public static class EventUtility
 	{
-		[MethodImpl(MethodImplOptions.NoInlining)]
 		public static void Raise(this EventHandler handler, object sender)
 		{
 			if (handler != null)
 				handler(sender, EventArgs.Empty);
 		}
-
-		[MethodImpl(MethodImplOptions.NoInlining)]
+		
 		public static void Raise<T>(this EventHandler<T> handler, object sender, T e) where T : EventArgs
 		{
 			if (handler != null)
