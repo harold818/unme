8eb574aa5b180b3ab3facc8b7106f51b8979bc36
sjalex <sjalex@83d443b3-9a48-0410-a4bc-37dee8c7bf04>
2009-05-22 04:47:00 +0000

    Added DelegateUtility class and tests.

---
+++ trunk/src/Unme.Common.Tests/DelegateUtilityTests.cs
@@ -0,0 +1,30 @@
+﻿using MbUnit.Framework;
+using System;
+
+namespace Unme.Common.Tests
+{
+	[TestFixture]
+	public class DelegateUtilityTests
+	{
+		[Test]
+		public void Cast()
+		{
+			Predicate<int> isPositive = n => n > 0;
+			Func<int, bool> isPositive2 = isPositive.Cast(typeof(Func<int, bool>)) as Func<int, bool>;
+
+			Assert.IsNotNull(isPositive2);
+			Assert.AreEqual(isPositive(3), isPositive2(3));
+			Assert.AreEqual(isPositive(-3), isPositive2(-3));
+		}
+
+		[Test]
+		public void Cast_Generic()
+		{
+			Predicate<int> isPositive = n => n > 0;
+			Func<int, bool> isPositive2 = isPositive.Cast<Func<int, bool>>();
+
+			Assert.AreEqual(isPositive(3), isPositive2(3));
+			Assert.AreEqual(isPositive(-3), isPositive2(-3));
+		}
+	}
+}
---
+++ trunk/src/Unme.Common/DelegateUtility.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+
+namespace Unme.Common
+{
+	/// <summary>
+	/// Delegate utlility methods.
+	/// </summary>
+	public static class DelegateUtility
+	{
+		/// <summary>
+		/// Casts the delegate to the specified type.
+		/// </summary>
+		public static T Cast<T>(this Delegate source) where T : class
+		{
+			return Cast(source, typeof(T)) as T;
+		}
+
+		/// <summary>
+		/// Casts the delegate to the specified type.
+		/// </summary>
+		public static Delegate Cast(this Delegate source, Type type)
+		{
+			if (source == null)
+				return null;
+
+			Delegate[] delegates = source.GetInvocationList();
+
+			if (delegates.Length == 1)
+				return Delegate.CreateDelegate(type, delegates[0].Target, delegates[0].Method);
+
+			Delegate[] delegatesDest = new Delegate[delegates.Length];
+			for (int nDelegate = 0; nDelegate < delegates.Length; nDelegate++)
+				delegatesDest[nDelegate] = Delegate.CreateDelegate(type, delegates[nDelegate].Target, delegates[nDelegate].Method);
+
+			return Delegate.Combine(delegatesDest);
+		}
+	}
+}
